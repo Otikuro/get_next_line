e4d0a4620be0226fb766d1981cb2cb6ee14c07c5
Juan Manuel Manrique Mendoza <juamanri@c2r8s2.42madrid.com>
2025-05-13 13:56:59 +0200

    start

--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juamanri <juamanri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/06 13:26:59 by juamanri          #+#    #+#             */
-/*   Updated: 2025/05/13 12:58:40 by juamanri         ###   ########.fr       */
+/*   Created: 2025/05/13 11:55:38 by juamanri          #+#    #+#             */
+/*   Updated: 2025/05/13 13:41:45 by juamanri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H_
-# define GET_NEXT_LINE_H_
-char	*get_next_line(int fd);
-#endif
+#include "./headers/get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	int	fd;
+	int	text;
+
+	fd = open("hola.txt", O_RDONLY);
+	while (text = get_next_line(fd))
+	{
+		printf("%s", text);
+	}
+	return (0);
+}
--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,7 +6,32 @@
 /*   By: juamanri <juamanri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:27:10 by juamanri          #+#    #+#             */
-/*   Updated: 2025/05/06 13:27:11 by juamanri         ###   ########.fr       */
+/*   Updated: 2025/05/13 13:55:18 by juamanri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../headers/get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	c;
+	int		byte;
+	int		line_len;
+
+	if (fd == -1)
+		return (0);
+	else
+	{
+		line_len = 0;
+		//El primer parametro es el file descriptor, elsegundo es la variable en el que se guarda la letra leida, el tercero es el numero de caracteres que se quieren leer.
+		byte = read(fd, &c, 1);
+		while (byte > 0)
+		{
+			line_len++;
+			byte = read(fd, &c, 1);
+			if (c == '\n')
+				byte == 0;
+		}
+		return (line_len);
+	}
+}
